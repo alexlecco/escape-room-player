3d99f94f982aa493790aa4473ab1fb57b8f85e9d
alexlecco <alexlecco24@gmail.com>
2020-06-15 22:43:10 -0300

    add sounds to start, penalize and finish events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import startSoundLoc from './sounds/start.wav'
+import penalizeSoundLoc from './sounds/penalize.wav'
+import timeoverSoundLoc from './sounds/timeover.wav'
+import checkpointSoundLoc from './sounds/checkpoint.wav' // no usado aun
 import ReactCountdownClock from 'react-countdown-clock'
 import firebase from './firestore'
 
@@ -8,7 +12,8 @@ export default class App extends Component {
     super(props)
     this.state = {
       time: 0,
-      clockColor: "#34ebab"
+      clockColor: '#34ebab',
+      isActive: false
     }
 
     this.room1Ref = firebase.firestore().collection('room1').doc('glYViwyAivdHSVtoljZN')
@@ -26,18 +31,39 @@ export default class App extends Component {
     }.bind(this))
   }
 
-  myCallback() { console.log("tiempo cumplido") } 
-  start() {this.setState({time: 3600, clockColor: "#34ebab"})}
-  reset() {this.setState({time: 0, clockColor: "#34ebab"})}
-  penalize() {this.setState({time: 300, clockColor: "#ff004c"})}
+  myCallback() {
+    let sound = new Audio(timeoverSoundLoc)
+    if(this.state.isActive) sound.play()
+    this.setState({time: 0, clockColor: '#34ebab'})
+    this.room1Ref.update({ "start": false, "penalization": false })
+  }
 
+  start() {
+    let sound = new Audio(startSoundLoc)
+    sound.play()
+    this.setState({
+      time: 3600,
+      clockColor: '#34ebab',
+      isActive: true
+    })
+  }
+
+  penalize() {
+    let sound = new Audio(penalizeSoundLoc)
+    sound.play()
+    this.setState({time: 300, clockColor: '#ff004c'})
+  }
+
+  reset() {this.setState({time: 0, clockColor: '#34ebab'})}
+  
   render() {
     const { time } = this.state
     const { clockColor } = this.state
+    const handleOnComplete = () => this.myCallback()
 
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className='App'>
+        <header className='App-header'>
           <h1> Bienvenido a Escape Room Tucumán </h1>
           <h2> PLAYER </h2>
           <p> tiempo restante: </p>
@@ -47,7 +73,7 @@ export default class App extends Component {
               color={clockColor}
               alpha={0.9}
               size={200}
-              onComplete={() => this.myCallback()}
+              onComplete={handleOnComplete}
             />
           </div>
         </header>
